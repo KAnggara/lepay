507135c0c0a15430acb1a680b6f31e3b2e454dbc
Call Vin <kanggara75@gmail.com>
2023-06-27 11:09:42 +0700

    add: setup wapi project

---
+++ wapi/src/app/logging.js
@@ -0,0 +1,7 @@
+import winston from "winston/lib/winston/config";
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.Console({})],
+});
