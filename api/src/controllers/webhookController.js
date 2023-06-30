fc85c3892e4d674ce83a75263820f9f0aba19118
Call Vin <kanggara75@gmail.com>
2023-06-30 18:41:54 +0700

    add: webhook

---
+++ api/src/controllers/webhookController.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+import 'dotenv/config';
+import { logger } from '../app/logging.js';
+import { prismaClient } from '../app/database.js';
+import { apiRequest } from '../services/services.js';
+
+const webhookIncomingMessage = async (req, res) => {
+  const body = req.body;
+  const payload = body.payload;
+
+  const text = payload.text;
+  const sender = payload.sender;
+  const device_id = payload.device_id;
+  const message_type = payload.message_type;
+  const webhook_type = body.webhook_type;
+
+  if (webhook_type === 'incoming_message') {
+    const check = await prismaClient.message.findFirst({
+      where: {
+        trigger: text,
+      },
+      select: {
+        state: true,
+        response: true,
+        hardwareId: true,
+      },
+    });
+
+    if (check === null) {
+      console.log('No response found');
+      res.status(204).end();
+    } else {
+      const data = {
+        phone_number: sender,
+        message: check.response,
+        device_id: device_id,
+        message_type: message_type,
+      };
+      if (check.hardwareId && check.state) {
+        await prismaClient.hardware.update({
+          where: {
+            id: check.hardwareId,
+          },
+          data: {
+            state: check.state,
+          },
+        });
+      }
+      const url = new URL(process.env.API_SERVER + '/messages');
+      const reqParams = {
+        token: process.env.API_TOKEN,
+        url: url,
+        method: 'POST',
+        payload: JSON.stringify(data),
+      };
+      try {
+        const { body, response } = await apiRequest(reqParams);
+        console.log(body);
+        res.status(response.statusCode).json(JSON.parse(body));
+      } catch (error) {
+        console.error('Something went wrong', {
+          body: error.body,
+          statusCode: error.response.statusCode,
+        });
+        res.status(error.response.statusCode).json(JSON.parse(error.body));
+      }
+    }
+  } else {
+    logger.error({ message: 'Unknown webhook type of ' + webhook_type });
+    res.status(400).end();
+  }
+};
+
+export { webhookIncomingMessage };
--- api/src/routes/public-api.js
+++ api/src/routes/public-api.js
@@ -7,11 +7,13 @@ import {
   deleteDevices,
 } from '../controllers/devicesController.js';
 
+import { webhookIncomingMessage } from '../controllers/webhookController.js';
 const publicRouter = new express.Router();
 
 publicRouter.post('/devices', addDevices);
 publicRouter.get('/devices', getAllDevices);
 publicRouter.get('/devices/:device_id', getDevices);
 publicRouter.delete('/devices/:device_id', deleteDevices);
+publicRouter.post('/webhook', webhookIncomingMessage);
 
 export { publicRouter };
