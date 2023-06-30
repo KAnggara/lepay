083749a99f75a85092d9ca01295b11cd0c927681
Call Vin <kanggara75@gmail.com>
2023-06-30 16:15:32 +0700

    add: database logging

---
+++ api/src/app/logging.js
@@ -0,0 +1,23 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'verbose',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: './logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: './logs/info.log',
+      level: 'info',
+    }),
+    new winston.transports.File({
+      filename: './logs/query.log',
+      level: 'verbose',
+    }),
+  ],
+});
