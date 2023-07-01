73cd675702f3131f8e54ae3ad877b194ac04b293
Call Vin <kanggara75@gmail.com>
2023-07-01 17:54:49 +0700

    add: #web refresh

--- web/index.php
+++ web/index.php
@@ -155,6 +155,9 @@ function timer($time)
     function close_window() {
       confirm("Close Window?");
     }
+    setTimeout(function() {
+      window.location.reload(1);
+    }, 5000);
   </script>
 </body>
 
