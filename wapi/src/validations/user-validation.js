4a942b3f2b6481c0b7b13bd77831a120587a8246
Call Vin <kanggara75@gmail.com>
2023-07-06 14:36:14 +0700

    fix: remove wapi

--- wapi/src/validations/user-validation.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi'
-
-const registerUserValidation = Joi.object({
-  username: Joi.string().min(6).max(250).required(),
-  password: Joi.string().min(6).max(250).required(),
-  email: Joi.string().email().required(),
-})
-
-export { registerUserValidation }
